bb031d1dae77b21646f56ec337feab8a3448a810
doomerxD <yusukeganba6@gmail.com>
2022-10-31 18:03:45 +0900

    投稿の編集機能追加

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,10 @@ Route::group(['middleware' => ['auth']], function(){
     Route::get('/posts/create', 'PostController@create');
     Route::get('/posts/{post}/result', 'ResultController@currentLocation')->name('result.currentLocation');
     Route::get('/posts/{post}', 'PostController@show');
+    Route::get('/posts/{post}/edit', 'PostController@edit');
+    Route::put('/posts/{post}', 'PostController@update');
     Route::post('/posts/create', 'PostController@upload');
     Route::delete('/posts/{post}', 'PostController@delete');
-    Route::get('/posts/{post}/edit', 'PostController@edit');
     
     
 });
